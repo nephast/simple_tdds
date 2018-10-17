35762532042661b6b43e59c967dcd7503aa1a1dd
Stéphan Gaud <stephgoeseast@gmail.com>
2018-10-17 23:35:37 +0100

    update with class instead of function

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,16 +1,37 @@
-const fizzbuzz = (num) => {
-  if (num%3 === 0) {
-    if (num%5 === 0) {
-      return 'fizzbuzz';
+// const fizzbuzz = (num) => {
+//   if (num%3 === 0) {
+//     if (num%5 === 0) {
+//       return 'fizzbuzz';
+//     }
+//     return 'fizz';
+//   }
+//   if (num%5 === 0) {
+//     return 'buzz';
+//   }
+//   return num;
+// };
+
+class Fizzbuzz {
+  constructor(num) {
+    this.num = num;
+  };
+
+  run() {
+    console.log('-=-=-', this.num)
+    if (this.num%3 === 0) {
+      if (this.num%5 === 0) {
+        return 'fizzbuzz';
+      }
+      return 'fizz'
+    } 
+    if (this.num%5 === 0) {
+      return 'buzz';
     }
-    return 'fizz';
+    return this.num;
   }
-  if (num%5 === 0) {
-    return 'buzz';
-  }
-  return num;
-};
+
+}
 
 module.exports = {
-  fizzbuzz
+  Fizzbuzz
 };
--- test/fizzbuzz.spec.js
+++ test/fizzbuzz.spec.js
@@ -1,26 +1,26 @@
 const chai = require('chai');
 
-const sut = require('../src/fizzbuzz');
+const { Fizzbuzz } = require('../src/fizzbuzz');
 const expect = chai.expect;
 
 describe('fizzbuzz', () => {
   it('should return 1 if 1 is entered', () => {
-    expect(sut.fizzbuzz(1)).to.equal(1);
+    expect(new Fizzbuzz(1).run()).to.equal(1);
   });
 
   it('should return 2 if 2 is entered', () => {
-    expect(sut.fizzbuzz(2)).to.equal(2);
+    expect(new Fizzbuzz(2).run()).to.equal(2);
   });
 
   it('should return fizz if 3 is entered', () => {
-    expect(sut.fizzbuzz(3)).to.equal('fizz')
+    expect(new Fizzbuzz(3).run()).to.equal('fizz')
   });
 
   it('should return buzz if 5 is entered', () => {
-    expect(sut.fizzbuzz(5)).to.equal('buzz');
+    expect(new Fizzbuzz(5).run()).to.equal('buzz');
   });
 
   it('should return fizzbuzz if 15 is entered', () => {
-    expect(sut.fizzbuzz(15)).to.equal('fizzbuzz');
+    expect(new Fizzbuzz(15).run()).to.equal('fizzbuzz');
   });
 });
