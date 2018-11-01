379f3d68dec8b9cc3196e6ee875cebe5b281998d
Stéphan Gaud <stephgoeseast@gmail.com>
2018-11-01 16:20:19 +0000

    test that we store lazily functions and arguments

--- src/lazy.js
+++ src/lazy.js
@@ -1,5 +1,10 @@
 class Lazy {
-
+  constructor() {
+    this.storage = [];
+  }
+  add(fn, ...args) {
+    return this.storage.push({[fn]: args})
+  }
 }
 
 module.exports = {
--- test/lazy.spec.js
+++ test/lazy.spec.js
@@ -1,2 +1,28 @@
 // add functions and parameters lazily
+// => add them to an array/object
+// call them when required
+// => check that each function is called
+// check final global result
+
+const chai = require('chai');
+expect = chai.expect;
+
+const { Lazy } = require('../src/lazy');
+
+describe('add function and their arguments to Lazy instance', () => {
+  it('should add an object to storage array', () => {
+    const lazy = new Lazy();
+    const fn = (...args) => {
+      return args.reduce((acc, val) => {
+        acc + val
+      }, 0)
+    };
+
+    const args = [2, 3, 3];
+
+    lazy.add(fn, ...args);
+    expect(lazy.storage.length).to.equal(1);
+    expect(lazy.storage[0]).to.have.key(fn);
+  })
+});
 
